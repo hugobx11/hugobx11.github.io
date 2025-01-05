50cae68c6b2f46300fa5c04db6a683fc11cba930
hugobx11 <149832100+hugobx11@users.noreply.github.com>
2025-01-05 13:53:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const serviceID = "service_knnf9k3";
+        const templateID = "template_6glqtz8";
+        
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(function() {
+                alert('Email sent successfully!');
+            }, function(error) {
+                alert('Failed to send email. Please try again.');
+            });
+    });
+});
+
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
